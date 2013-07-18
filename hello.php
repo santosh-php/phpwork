c60c2dad68d723a187cc5de6226ec1c340a2e456
santos-php <santoshkumar41189@gmail.com>
2013-07-18 18:43:21 +0530

    This is just the Layout Page

---
+++ hello.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+<title>
+Hello Php
+</title>
+<style type="text/css">
+#nav{
+
+
+border:1px solid teal;
+height:80%;
+width:135px;
+background:gray;
+float:left;
+}
+#nav ul{
+
+display:block;
+}
+#nav ul li{
+
+text-decoration:none;
+color:white;
+}
+</style>
+</head>
+<body>
+
+<div id ="head" style="border:1px solid teal;height:90px;width:100%;background:orange;">
+<center><i><h3>Welcome to my Php page</h3></i></center>
+</div>
+
+<div id="nav">
+<ul>
+<li>
+<a href="#">Home</a><br/></li>
+<li><a href="#">Php</a><br/></li>
+<li><a href="#">Carrier</a><br/></li>
+<li><a href="#">About Us</a><br/></li>
+<li><a href="#">Contact Us</a><br/></li>
+</ul>
+</div>
+
+<div id="content"style="border:2px solid red;width:70%;height:78%;position:absolute;top:14%;left:14%;">
+<h4>Content Region</h4>
+</div>
+
+<div id="sidebar" style="border:1px solid teal;height:80%;width:135px; background:blue;position:absolute;top:14%;left:85%;color:white;">
+<h4>Latest New here</h4>
+<h4>Latest New here</h4>
+<h4>Latest New here</h4>
+<h4>Latest New here</h4>
+<h4>Latest New here</h4>
+</div>
+
+<div id ="footer" style="border:1px solid teal;height:70px;width:98%;background:orange;position:absolute;top:89%;">
+<center><i><h3>This is Footer</h3></i></center>
+</div>
+</body>
+</html>
